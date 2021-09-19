6a713b288896131d3731769e69ef02b1c4872e61
覃勇涛 <2579137823@qq.com>
2021-09-19 22:18:24 +0800

    用户登录-创建页面组件并配置路由

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,7 @@
 <template>
   <div id="app">
-    <h1>我的第一个Vue项目</h1>
     <!-- 路由出口 -->
     <router-view />
-    <el-button type="primary">主要按钮</el-button>
-    <el-button type="success"
-               round>成功按钮</el-button>
-
-    <el-input-number v-model="num"
-                     @change="handleChange"
-                     :min="1"
-                     :max="10"
-                     label="描述文字"></el-input-number>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+// 在VueCLI创建的项目中 @表示src目录
+// 它是src目录的路径别名
+// 建议：如果加载的资源路径就在当前目录下 那就正常写
+// 如果需要进行父级路径查找都是用@
+import Login from '@/views/login'
+
 Vue.use(VueRouter)
 
-const routes = [
-  //  路由配置表
-]
+//  路由配置表
+const routes = [{
+  path: '/login',
+  name: 'login',
+  component: Login
+}]
 
 const router = new VueRouter({
   routes
