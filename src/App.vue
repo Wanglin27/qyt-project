d11f670197e2ae4d4a5687a19520cafe91db62f6
覃勇涛 <2579137823@qq.com>
2021-09-19 21:20:37 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,29 @@
   <div id="app">
     <h1>我的第一个Vue项目</h1>
     <!-- 路由出口 -->
-    <router-view/>
+    <router-view />
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="success"
+               round>成功按钮</el-button>
+
+    <el-input-number v-model="num"
+                     @change="handleChange"
+                     :min="1"
+                     :max="10"
+                     label="描述文字"></el-input-number>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      num: 6
+    }
+  }
 }
 </script>
 
 <style lang="less">
-
 </style>
---
+++ src/utils/request.js
@@ -0,0 +1,12 @@
+// 基于 axios 封装的请求模块
+
+import axios from 'axios'
+
+// 创建一个axios实例，说白了就是复制了一个axios
+// 我们通过这个实例去发请求，把需要的配置配置给这个实例处理
+const request = axios.create({
+  baseURL: 'http://api-toutiao-web.itheima.net' // 请求的基础路径
+})
+
+// 导出请求方法
+export default request
